5f0a90e066ac01ae60a27e7c9633d857bb284fb8
Fayed <aa.abdo20000@gmail.com>
2024-01-07 16:26:31 +0800

    All are ready with the user-interface

--- flask_application/yolo_image.py
+++ flask_application/yolo_image.py
@@ -15,62 +15,6 @@ def image_detection(path_x):
         source=frame,
         show=True, conf=0.8)
 
-    # bottles = 0
-    # for i in results[0].boxes.cls:
-    #     if i == 0:
-    #         bottles += 1
-    # bottles_missing = 24 - bottles
-    #
-    # if bottles_missing:
-    #     message = f"{bottles_missing} bottles are missing"
-    #     cv2.putText(img, message, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    #
-    # yield img
-
-    ##
-
-    # for frame in results.render():  # Iterate over rendered frames
-    #     bottles = 0
-    #     for i in frame.boxes.cls:
-    #         if i == 0:
-    #             bottles += 1
-    #     bottles_missing = 24 - bottles
-    #
-    #     if bottles_missing:
-    #         message = f"{bottles_missing} bottles are missing"
-    #         cv2.putText(frame, message, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    #
-    #     yield frame
-
-    # classNames = ['person', 'bicycle', 'car', 'motorbike', 'aeroplane', 'bus', 'train', 'truck', 'boat',
-    #               'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat',
-    #               'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella',
-    #               'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat',
-    #               'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
-    #               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli',
-    #               'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'sofa', 'pottedplant', 'bed',
-    #               'diningtable', 'toilet', 'tvmonitor', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
-    #               'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-    #               'teddy bear', 'hair drier', 'toothbrush']
-    #
-    # for r in results:
-    #     boxes = r.boxes
-    #     for box in boxes:
-    #         x1, y1, x2, y2 = box.xyxy[0]
-    #         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-    #         print(x1, y1, x2, y2)
-    #         cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
-    #         conf = math.ceil((box.conf[0] * 100)) / 100
-    #         cls = int(box.cls[0])
-    #         class_name = classNames[cls]
-    #         label = f'{class_name}{conf}'
-    #         t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
-    #         # print(f">>>>{t_size}")
-    #         c2 = x1 + t_size[0], y1 - t_size[1] - 3
-    #         cv2.rectangle(frame, (x1, y1), c2, [255, 0, 255], -1, cv2.LINE_AA)
-    #         cv2.putText(frame, label, (x1, y1 - 2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
-
-    ##
     bottles = 0
     for i in results[0].boxes.cls:
         if i == 0:
@@ -88,9 +32,17 @@ def image_detection(path_x):
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
 
-    if bottles_missing:
+    if bottles_missing > 1:
         show_alert(f"{bottles_missing} bottles are missing")
+    elif bottles_missing == 1:
+        show_alert(f"{bottles_missing} bottle is missing")
+    elif bottles_missing == 0:
+        cv2.putText(img, "Package ready", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 10), 3)
+    elif bottles_missing < 0:
+        cv2.putText(img, f"Watch out! more than required by {bottles_missing * -1} ", (50, 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
 
-    # cv2.imshow("Image", frame)
+
+    cv2.imshow("Image", frame)
     yield img
 # cv2.destroyAllWindows()
--- flask_application/yolo_video.py
+++ flask_application/yolo_video.py
@@ -16,17 +16,49 @@ def video_detection(path_x):
     # current_frame_number = int(cap.get(1))
 
     # model = YOLO("D:/Fayed/UTM/Sem8/FYP2/dataset/generated_model/best.pt")
-    model = YOLO("../yolov8n.pt")
-    classNames = ['person', 'bicycle', 'car', 'motorbike', 'aeroplane', 'bus', 'train', 'truck', 'boat',
-                  'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat',
-                  'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella',
-                  'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat',
-                  'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
-                  'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli',
-                  'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'sofa', 'pottedplant', 'bed',
-                  'diningtable', 'toilet', 'tvmonitor', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
-                  'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-                  'teddy bear', 'hair drier', 'toothbrush']
+    #///
+    # model = YOLO("../yolov8n.pt")
+    # classNames = ['person', 'bicycle', 'car', 'motorbike', 'aeroplane', 'bus', 'train', 'truck', 'boat',
+    #               'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat',
+    #               'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella',
+    #               'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat',
+    #               'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+    #               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli',
+    #               'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'sofa', 'pottedplant', 'bed',
+    #               'diningtable', 'toilet', 'tvmonitor', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    #               'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+    #               'teddy bear', 'hair drier', 'toothbrush']
+    #
+    # while True:
+    #     ret, frame = cap.read()
+    #     print(f"vid frame: {frame}")
+    #
+    #     if not ret:
+    #         break
+    #
+    #     results = model(frame, stream=True)
+    #     for r in results:
+    #         boxes = r.boxes
+    #         for box in boxes:
+    #             x1, y1, x2, y2 = box.xyxy[0]
+    #             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+    #             print(x1, y1, x2, y2)
+    #             cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
+    #             conf = math.ceil((box.conf[0] * 100)) / 100
+    #             cls = int(box.cls[0])
+    #             class_name = classNames[cls]
+    #             label = f'{class_name}{conf}'
+    #             t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+    #             # print(f">>>>{t_size}")
+    #             c2 = x1 + t_size[0], y1 - t_size[1] - 3
+    #             cv2.rectangle(frame, (x1, y1), c2, [255, 0, 255], -1, cv2.LINE_AA)
+    #             cv2.putText(frame, label, (x1, y1 - 2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
+    #///
+
+    model = YOLO("D:/Fayed/UTM/Sem8/FYP2/dataset/generated_model/best.pt")
+
+    frame_counter = 0
+    skip_frames = 5  # Process every 5th frame
 
     while True:
         ret, frame = cap.read()
@@ -35,28 +67,42 @@ def video_detection(path_x):
         if not ret:
             break
 
-        results = model(frame, stream=True)
-        for r in results:
-            boxes = r.boxes
-            for box in boxes:
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                print(x1, y1, x2, y2)
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
-                conf = math.ceil((box.conf[0] * 100)) / 100
-                cls = int(box.cls[0])
-                class_name = classNames[cls]
-                label = f'{class_name}{conf}'
-                t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
-                # print(f">>>>{t_size}")
-                c2 = x1 + t_size[0], y1 - t_size[1] - 3
-                cv2.rectangle(frame, (x1, y1), c2, [255, 0, 255], -1, cv2.LINE_AA)
-                cv2.putText(frame, label, (x1, y1 - 2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
+        frame_counter += 1
 
-        #cv2.imshow("Image", frame)
+        if frame_counter % skip_frames != 0:
+            continue  # Skip frames if not the desired frame to process
+
+        results = model.predict(
+            source=frame,
+            show=True, conf=0.8)
+
+        bottles = 0
+        for detection in results:
+            boxes_cls_list = list(detection.boxes.cls)
+            for i in boxes_cls_list:
+                if i == 0:
+                    bottles += 1
+
+        bottles_missing = 24 - bottles
+
+        img = frame  # Assuming 'frame' is the original frame
+
+        def show_alert(message):
+            cv2.putText(img, message, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        if bottles_missing > 1:
+            show_alert(f"{bottles_missing} bottles are missing")
+        elif bottles_missing == 1:
+            show_alert(f"{bottles_missing} bottle is missing")
+        elif bottles_missing == 0:
+            cv2.putText(img, "Package ready", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        elif bottles_missing < 0:
+            cv2.putText(img, f"Watch out! more than required by {bottles_missing*-1} ", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+
+        cv2.imshow("Image", frame)
+        yield img
         yield frame
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-            #break
-cv2.destroyAllWindows()
 
+    cv2.destroyAllWindows()
 
